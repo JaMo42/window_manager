e879dcefcc2e64c86eccacba39e1b24bed4fc04f
Jakob <jakob@mohrbacher.eu>
2022-05-09 13:24:47 +0200

    compress motion events when not performing an action

--- src/event.rs
+++ src/event.rs
@@ -48,6 +48,17 @@ pub unsafe fn motion (event: &XButtonEvent) {
     }
     mouse_held = 0;
   }
+  else {
+    // Ignore all subsequent MotionNotify events
+    let mut my_event: XEvent = uninitialized! ();
+    loop {
+      XNextEvent (display, &mut my_event);
+      if my_event.type_ != MotionNotify {
+        break;
+      }
+    }
+    XPutBackEvent (display, &mut my_event);
+  }
 }
 
 
