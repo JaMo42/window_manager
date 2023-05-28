JaMo42/window_manager

commit 129320e0face738021d9599cc36bb0754b630f81

add bar height to top padding of main monitor

src/monitors.rs
@@ -137,6 +137,7 @@ impl PartialEq for Monitor {
 pub struct Monitors {
     monitors: Vec<Monitor>,
     primary: usize,
+    bar_height: u16,
 }
 
 impl Monitors {
@@ -147,6 +148,7 @@ impl Monitors {
         Self {
             monitors: Vec::new(),
             primary: 0,
+            bar_height: 0,
         }
     }
 
@@ -207,6 +209,13 @@ impl Monitors {
                 secondary_padding
             });
         }
+    }
+
+    pub fn set_bar_height(&mut self, height: u16) {
+        let p = &mut self.monitors[self.primary];
+        p.window_area.y -= self.bar_height as i16;
+        p.window_area.y += height as i16;
+        self.bar_height = height;
     }
 
     /// Print monitors info to log.

src/bar/bar.rs
@@ -8,7 +8,7 @@ use crate::{
     error::OrFatal,
     event::Signal,
     ewmh::{self, WindowType},
-    monitors::monitors,
+    monitors::monitors_mut,
     rectangle::Rectangle,
     window_manager::{WindowKind, WindowManager},
     x::{Window, XcbWindow},
@@ -46,7 +46,7 @@ impl Bar {
             .drawing_context
             .lock()
             .font_height(Some(&config.bar.font));
-        let monitors = monitors();
+        let mut monitors = monitors_mut();
         let monitor = monitors.primary();
         let height = config.bar.height.resolve(
             Some(monitor.dpmm()),
@@ -56,6 +56,7 @@ impl Bar {
         let visual = *display.truecolor_visual();
         let mut geometry = *monitor.geometry();
         geometry.height = height;
+        monitors.set_bar_height(height);
         let window = Window::builder(display)
             .geometry(geometry)
             .depth(visual.depth)
@@ -247,7 +248,7 @@ impl Bar {
     }
 
     pub fn resize(&mut self) {
-        let monitors = monitors();
+        let mut monitors = monitors_mut();
         let monitor = monitors.primary();
         let font_height = self
             .wm
@@ -266,6 +267,7 @@ impl Bar {
         }
         self.geometry = monitor.geometry().with_height(height);
         self.window.resize(self.geometry.width - width, height);
+        monitors.set_bar_height(height);
         self.wm
             .signal_sender
             .send(Signal::UpdateBar(true))

commit 834bf3f2ec59d5887c72bc82e50d39b0f5b14154

fix invalid primary window area caused by 129320e

src/cfg/error.rs
@@ -98,7 +98,7 @@ impl Error {
     }
 
     pub fn to_markup(&self, path: &str, source: &str) -> String {
-        let line = self.line.expect("error has no line information") ;
+        let line = self.line.expect("error has no line information");
         let mut builder = Report::build(ReportKind::Error, path, line).with_message(&self.message);
         if let Some((span, msg)) = self.label.clone() {
             builder.add_label(

src/monitors.rs
@@ -104,7 +104,6 @@ impl Monitor {
     }
 
     pub fn window_area(&self) -> &Rectangle {
-        assert!(self.geometry != self.window_area);
         &self.window_area
     }
 
@@ -213,7 +212,7 @@ impl Monitors {
 
     pub fn set_bar_height(&mut self, height: u16) {
         let p = &mut self.monitors[self.primary];
-        p.window_area.y -= self.bar_height as i16;
+        p.window_area.height -= height;
         p.window_area.y += height as i16;
         self.bar_height = height;
     }