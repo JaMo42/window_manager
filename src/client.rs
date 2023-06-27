JaMo42/window_manager

commit 862242da8413092ba547c15e1949f029418ae870

fix stacking of extended window frames

src/client.rs
@@ -29,8 +29,8 @@ use std::{
 };
 use xcb::{
     x::{
-        Atom, ClientMessageData, ClientMessageEvent, ConfigureNotifyEvent, Cursor, EventMask,
-        Timestamp, CURRENT_TIME,
+        Atom, ClientMessageData, ClientMessageEvent, ConfigWindow, ConfigureNotifyEvent,
+        ConfigureWindow, Cursor, EventMask, StackMode, Timestamp, CURRENT_TIME,
     },
     Xid,
 };
@@ -339,6 +339,30 @@ impl Client {
     pub fn raise(&self) {
         self.frame.raise();
         self.extended_frame.restack(self);
+    }
+
+    /// Ensures the extended frame of `self` is correctly stacked above `other`.
+    /// I don't know why we can't just raise out extended frame but need to lower
+    /// the other window instead but this is the only way I got it to work.
+    pub fn ensure_stacked_above(&self, other: &Client) {
+        let my_extended_frame = match self.extended_frame.handle() {
+            Some(handle) => handle,
+            // if we don't have an extended handle no further action is required
+            None => return,
+        };
+        let stack_below = |handle| {
+            self.display().void_request(&ConfigureWindow {
+                window: handle,
+                value_list: &[
+                    ConfigWindow::Sibling(my_extended_frame),
+                    ConfigWindow::StackMode(StackMode::Below),
+                ],
+            });
+        };
+        if let Some(extended_frame) = other.extended_frame.handle() {
+            stack_below(extended_frame);
+        }
+        stack_below(other.frame.handle());
     }
 
     pub fn is_on_active_workspace(&self) -> bool {

src/extended_frame.rs
@@ -58,6 +58,14 @@ impl ExtendedFrame {
         self.window.destroy(display);
     }
 
+    pub fn handle(&self) -> Option<XcbWindow> {
+        if self.extents == 0 {
+            None
+        } else {
+            Some(self.window.handle())
+        }
+    }
+
     pub fn handle_eq(&self, handle: XcbWindow) -> bool {
         self.extents != 0 && handle == self.window.handle()
     }

src/main_event_sink.rs
@@ -205,6 +205,9 @@ impl MainEventSink {
             client.focus();
             self.pressed_button = event.detail();
             let mut workspace = self.wm.workspace(client.workspace());
+            if let Some(prev) = workspace.focused() {
+                client.ensure_stacked_above(prev);
+            }
             workspace.focus(child);
         }
     }

commit bce74942e91cb902720de6f27a832e22f462ea81

fix wrong stacking order when focusing windows
this was broken by 862242d because we only cared about the top 2 windows
on the stack which messed up the order by showing some lower windows
above upper windows.

src/client.rs
@@ -29,8 +29,8 @@ use std::{
 };
 use xcb::{
     x::{
-        Atom, ClientMessageData, ClientMessageEvent, ConfigWindow, ConfigureNotifyEvent,
-        ConfigureWindow, Cursor, EventMask, StackMode, Timestamp, CURRENT_TIME,
+        Atom, ClientMessageData, ClientMessageEvent, ConfigureNotifyEvent, Cursor, EventMask,
+        Timestamp, CURRENT_TIME,
     },
     Xid,
 };
@@ -352,30 +352,6 @@ impl Client {
     pub fn raise(&self) {
         self.frame.raise();
         self.extended_frame.restack(self);
-    }
-
-    /// Ensures the extended frame of `self` is correctly stacked above `other`.
-    /// I don't know why we can't just raise out extended frame but need to lower
-    /// the other window instead but this is the only way I got it to work.
-    pub fn ensure_stacked_above(&self, other: &Client) {
-        let my_extended_frame = match self.extended_frame.handle() {
-            Some(handle) => handle,
-            // if we don't have an extended handle no further action is required
-            None => return,
-        };
-        let stack_below = |handle| {
-            self.display().void_request(&ConfigureWindow {
-                window: handle,
-                value_list: &[
-                    ConfigWindow::Sibling(my_extended_frame),
-                    ConfigWindow::StackMode(StackMode::Below),
-                ],
-            });
-        };
-        if let Some(extended_frame) = other.extended_frame.handle() {
-            stack_below(extended_frame);
-        }
-        stack_below(other.frame.handle());
     }
 
     pub fn is_on_active_workspace(&self) -> bool {

src/workspace.rs
@@ -7,10 +7,11 @@ use crate::{
     window_manager::WindowManager,
     x::{Display, Window, XcbWindow},
 };
+use itertools::Itertools;
 use std::sync::{mpsc::Sender, Arc};
 use x11::keysym::{XK_Alt_L, XK_Tab};
 use xcb::{
-    x::{EventMask, KeyButMask, KeyPressEvent},
+    x::{ConfigWindow, ConfigureWindow, EventMask, KeyButMask, KeyPressEvent, StackMode},
     Event, Xid,
 };
 
@@ -115,6 +116,43 @@ impl Workspace {
         panic!("Tried to remove client no on workspace");
     }
 
+    /// Restacks all clients in this workspace.
+    /// For some reason this is neccessary to keep proper stacking order while
+    /// also keeping the correct window order for extended frames because we
+    /// we can't just raise the extended frame of the most recently focused
+    /// window.
+    fn restack(&self) {
+        if self.clients.len() < 2 {
+            return;
+        }
+        let stack = |upper, lower| {
+            self.display
+                .try_void_request(&ConfigureWindow {
+                    window: lower,
+                    value_list: &mut [
+                        ConfigWindow::Sibling(upper),
+                        ConfigWindow::StackMode(StackMode::Below),
+                    ],
+                })
+                .unwrap();
+        };
+        self.clients[0].frame().raise();
+        if let Some(exframe) = self.clients[0].extended_frame().handle() {
+            stack(self.clients[0].frame().handle(), exframe);
+        }
+        for (upper, lower) in self.clients.iter().tuple_windows() {
+            let upper = upper
+                .extended_frame()
+                .handle()
+                .unwrap_or_else(|| upper.frame().handle());
+            let lower_frame = lower.frame().handle();
+            stack(upper, lower_frame);
+            if let Some(exframe) = lower.extended_frame().handle() {
+                stack(lower_frame, exframe);
+            }
+        }
+    }
+
     /// Focus the client at the given index.
     /// Emits a `FocusClient` signal.
     pub fn focus_at(&mut self, idx: usize) {
@@ -131,9 +169,7 @@ impl Workspace {
             self.clients.insert(0, c);
         }
         self.focus_client(&self.clients[0]);
-        if let Some(prev) = self.clients.get(1) {
-            self.clients[0].ensure_stacked_above(prev);
-        }
+        self.restack();
     }
 
     /// Focus the client with the given window.